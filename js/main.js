770d74a08cb03098039e211885c19f8855505442
Tina Holly (Tinacious Design) <tinacious.design@gmail.com>
2013-08-10 13:11:31 -0400

    added table with tabs

--- js/main.js
+++ js/main.js
@@ -9,5 +9,25 @@ $(document).ready(function(){
 
 	// Count Animated 
 	$('.number').animateNumbers(244565300, true, 900000);
+
+	// Tabs
+	$('.tab-content').hide();
+	$('.tab-content').first().show();
+	$('.tabs-nav li a').first().addClass('active-tab')
+	$('.tabs-nav li a').click(function(e){
+	  $('.tab-content').hide();
+	  $('.tabs-nav a').removeClass('active-tab');
+	  $(this).addClass('active-tab');
+	  var activeTab = $(this).attr('href');
+	  $(activeTab).show();
+	  e.preventDefault();
+	});
+
+		// Add course tab
+		$('#modalAddCourse').hide();
+		$('.addCourse').fancybox({
+			maxWidth: 960
+		});
+
 });
 
